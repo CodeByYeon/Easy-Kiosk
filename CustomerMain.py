94afb40eadc935ecf8bd311bda283181396dc4b2
Yeon <nicegod95@gmail.com>
2024-07-16 21:17:14 +0900

    Second Commit

--- CustomerMain.py
+++ CustomerMain.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 def makezoom(text):
+    #텍스트 크기 조절하는 버튼 생성
     btn = QPushButton()
     btn.setFixedSize(60, 60)
     btn.setStyleSheet(
@@ -11,21 +12,37 @@ def makezoom(text):
     btn.setText(text)
     return btn
 
-def btnSetNHome(w, h, img):
-    # 세팅버튼 및 홈버튼 정의
+def btnmake(w,h,text,parents):
+    #버튼 생성하는 함수
     btn = QPushButton()
-    btn.setStyleSheet(
-        f"QPushButton {{ background-image: url('{img}');"
-        f"background-position: center;"
-        f"background-repeat: no-repeat;"
-        f"background-size: cover;}}")
     btn.setFixedSize(w, h)
+    btn.setText(text)
+    btn.setParents(parents)
     btn.setFlat(1)
     return btn
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setStyleSheet(u"Background-color:rgb(255, 255, 255);")
+        self.setFixedSize(480,830)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        #바탕이 되는 스택위젯 생성
+        self.StackWidgetMain = QStackedWidget(self)
+        self.setCentralWidget(self.StackWidgetMain)
+        self.StackWidgetMain.setFixedSize(480,830)
+
+        #첫번째 페이지
+        self.WidgetEatWhere = QWidget(self.StackWidgetMain)
+        self.WidgetEatWhere.setFixedSize(480,830)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
