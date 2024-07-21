a8526ee1b6f61808a224b48974196740ad9bb600
Yeon <nicegod95@gmail.com>
2024-07-21 16:28:09 +0900

    edit function

--- CustomerMain.py
+++ CustomerMain.py
@@ -3,6 +3,10 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+fontTitle = QFont("SUITE SEMI Bold", 35)
+fontMiddle = QFont("SUITE",25)
+fontSmall = QFont("SUITE",16)
+fontCount = 0
 
 def makeBtn(w, h, text, parents):
     # 버튼 생성하는 함수
@@ -36,9 +40,8 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.WidgetMain)
         self.WidgetMain.setFixedSize(480, 830)
 
-        LabelWelcomeMain = makeLabel(350, 85, "선택해주세요", self.WidgetMain)
-        LabelWelcomeMain.setGeometry(10, 35, 350, 85)
-        LabelWelcomeMain.setFont(QFont("Arial", 39))
+        self.LabelWelcomeMain = makeLabel(350, 85, "선택해주세요", self.WidgetMain)
+        self.LabelWelcomeMain.setGeometry(10, 35, 350, 85)
 
         def makezoom(text):
             # 텍스트 크기 조절하는 버튼 생성
@@ -51,13 +54,15 @@ class MainWindow(QMainWindow):
             return btn
 
         LabelZoom = makeLabel(105, 30, "글씨 크기", self.WidgetMain)
-        LabelZoom.setFont(QFont("Arial", 19))
         LabelZoom.setGeometry(365, 35, 105, 30)
 
         BtnZoomIn = makezoom("+")
         BtnZoomOut = makezoom("-")
         BtnZoomIn.setGeometry(365, 70, 50, 50)
         BtnZoomOut.setGeometry(420, 70, 50, 50)
+        BtnZoomIn.clicked.connect(self.increasefont)
+        BtnZoomOut.clicked.connect(self.decreasefont)
+
 
         # 바탕이 되는 스택위젯 생성
         self.StackWidgetMain = QStackedWidget(self.WidgetMain)
@@ -68,6 +73,13 @@ class MainWindow(QMainWindow):
         self.StackWidgetMain.addWidget(self.WidgetEatWhere)
         self.StackWidgetMain.setCurrentWidget(self.WidgetOrderMethod)
 
+        self.BtnTakeout.setFont(fontMiddle)
+        self.BtnHere.setFont(fontMiddle)
+        self.VoiceOrderbtn.setFont(fontMiddle)
+        self.NormalOrderbtn.setFont(fontMiddle)
+        self.RecommendOrderbtn.setFont(fontMiddle)
+        self.LabelWelcomeMain.setFont(fontTitle)
+
     def pageMethodUI(self):
         # 첫번째 페이지
         self.WidgetOrderMethod = QWidget(self.StackWidgetMain)
@@ -76,18 +88,18 @@ class MainWindow(QMainWindow):
         FrameOrderMethod.setGeometry(QRect(0, 10, 480, 660))
         LayoutOrderMethod = QGridLayout(FrameOrderMethod)
 
-        NormalOrderbtn = makeBtn(440, 190, "일반 주문", FrameOrderMethod)
-        VoiceOrderbtn = makeBtn(440, 190, "음성 주문", FrameOrderMethod)
-        RecommendOrderbtn = makeBtn(440, 190, "메뉴 추천", FrameOrderMethod)
+        self.NormalOrderbtn = makeBtn(440, 190, "일반 주문", FrameOrderMethod)
+        self.VoiceOrderbtn = makeBtn(440, 190, "음성 주문", FrameOrderMethod)
+        self.RecommendOrderbtn = makeBtn(440, 190, "메뉴 추천", FrameOrderMethod)
 
         # 버튼에 클릭 이벤트 연결
-        NormalOrderbtn.clicked.connect(lambda: self.selectOrderMethod("일반 주문"))
-        VoiceOrderbtn.clicked.connect(lambda: self.selectOrderMethod("음성 주문"))
-        RecommendOrderbtn.clicked.connect(lambda: self.selectOrderMethod("메뉴 추천"))
+        self.NormalOrderbtn.clicked.connect(lambda: self.selectOrderMethod("일반 주문"))
+        self.VoiceOrderbtn.clicked.connect(lambda: self.selectOrderMethod("음성 주문"))
+        self.RecommendOrderbtn.clicked.connect(lambda: self.selectOrderMethod("메뉴 추천"))
 
-        LayoutOrderMethod.addWidget(NormalOrderbtn, 0, 0)
-        LayoutOrderMethod.addWidget(VoiceOrderbtn, 1, 0)
-        LayoutOrderMethod.addWidget(RecommendOrderbtn, 2, 0)
+        LayoutOrderMethod.addWidget(self.NormalOrderbtn, 0, 0)
+        LayoutOrderMethod.addWidget(self.VoiceOrderbtn, 1, 0)
+        LayoutOrderMethod.addWidget(self.RecommendOrderbtn, 2, 0)
         LayoutOrderMethod.setVerticalSpacing(45)
 
     def pageWhereUI(self):
@@ -95,16 +107,40 @@ class MainWindow(QMainWindow):
         self.WidgetEatWhere = QWidget(self.StackWidgetMain)
         self.WidgetEatWhere.setFixedSize(480, 700)
 
-        BtnHere = makeBtn(210, 500, "먹고가기", self.WidgetEatWhere)
-        BtnTakeout = makeBtn(210, 500, "포장하기", self.WidgetEatWhere)
-        BtnHere.setGeometry(25, 140, 200, 500)
-        BtnTakeout.setGeometry(255, 140, 200, 500)
+        self.BtnHere = makeBtn(210, 500, "먹고가기", self.WidgetEatWhere)
+        self.BtnTakeout = makeBtn(210, 500, "포장하기", self.WidgetEatWhere)
+        self.BtnHere.setGeometry(25, 140, 200, 500)
+        self.BtnTakeout.setGeometry(255, 140, 200, 500)
 
     def selectOrderMethod(self, method):
         self.order_method = method  # 선택한 주문 방법 저장
         print(f"선택한 주문 방법: {self.order_method}")  # 콘솔에 출력 (테스트용)
         self.StackWidgetMain.setCurrentWidget(self.WidgetEatWhere)  # 두 번째 페이지로 전환
 
+    def updateFont(self):
+        self.BtnTakeout.setFont(fontMiddle)
+        self.BtnHere.setFont(fontMiddle)
+        self.VoiceOrderbtn.setFont(fontMiddle)
+        self.NormalOrderbtn.setFont(fontMiddle)
+        self.RecommendOrderbtn.setFont(fontMiddle)
+        self.LabelWelcomeMain.setFont(fontTitle)
+
+    def increasefont(self):
+        global fontCount
+        if fontCount < 5 :
+            fontCount += 1
+            fontTitle.setPointSize(fontTitle.pointSize()+1)
+            fontMiddle.setPointSize(fontMiddle.pointSize()+1)
+            fontSmall.setPointSize(fontSmall.pointSize()+1)
+            self.updateFont()
+    def decreasefont(self):
+        global fontCount
+        if fontCount > -5 :
+            fontCount -= 1
+            fontTitle.setPointSize(fontTitle.pointSize()-1)
+            fontMiddle.setPointSize(fontMiddle.pointSize()-1)
+            fontSmall.setPointSize(fontSmall.pointSize()-1)
+            self.updateFont()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
